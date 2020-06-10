a96080fda6fa4adb61252c20523c60ac09ccd959
Siddhu Kaulapure <siddhu.kaulapure@atos.net>
2020-06-10 18:19:32 +0530

    added delete functionality

--- src/components/ImageItem/ImageItemHooks.js
+++ src/components/ImageItem/ImageItemHooks.js
@@ -1,22 +1,22 @@
 import './ImageItem.css';
 import React from 'react';
-import Services from '../../Services/Services';
 
 {/* This function will draw the individual image. */}
 export default function ImageItemHooks({imagedata}){
 
-    function removeImage(id){
-        //Services.deleteImage(id);
-        console.log(id);
+    const divRef = React.useRef()
+
+    function removeImage(){
+        divRef.current.remove();
     }
 
         return (
             (imagedata)?
-            <figure className="imgContainer" > 
+            <figure className="imgContainer" ref={divRef}> 
                 <img src={imagedata.cover_photo.urls.small} alt={imagedata.cover_photo.alt_description} />
                 <div className="imageInfoBar">
                     <span>{imagedata.cover_photo.likes} Likes</span>
-                    <button className="imageDeleteBtn" onClick={() => removeImage(imagedata.id)}>Delete</button>
+                    <button className="imageDeleteBtn" onClick={() => removeImage()}>Delete</button>
                 </div>
             </figure>
             :null
